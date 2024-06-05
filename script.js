58a515587e5c9f1e232a449aa17ef7167af20b87
piyush kumar <77287090+Piyush-kumar-310@users.noreply.github.com>
2024-06-05 19:52:21 +0530

    finished the sketch logic

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 const pad = document.querySelector(".pad");
-const padWidth = 400;
-const padHeight = 400;
+const padWidth = 500;
+const padHeight = 500;
 
 let gridSize = 16;
 
-gridMaker(gridSize);
+let colorPicker = "red";
+
+const menu = document.querySelector("#size-menu");
+
+menu.addEventListener("change", event => {
+    while(pad.firstChild){
+        pad.removeChild(pad.firstChild);
+    }
+
+    let selectedOption = event.target.value;
+    console.log(selectedOption);
+
+    if(selectedOption === "8"){
+        gridSize = 8;
+    }
+    else if(selectedOption === "default" || selectedOption === "16"){
+        gridSize = 16;
+    }
+    else if(selectedOption === "64"){
+        gridSize = 64;
+    }
+    else {
+        gridSize = parseInt(prompt("Enter a number between 1 and 100"));
+    }
+
+    gridMaker(gridSize);
+    addEventListenerToCells();
+});
 
 function gridMaker(num) {
     for(let i = 0; i<num; i++){
@@ -18,9 +45,26 @@ function gridMaker(num) {
             const row = document.createElement("div");
             row.classList.add("cell-row");
 
-            row.style.height = (padHeight/num) + "p";
+            row.style.height = (padHeight/num) + "px";
             row.style.flex = "1";
             column.appendChild(row);
         }
     }
 }
+
+function addEventListenerToCells() {
+    const cells = document.querySelectorAll(".cell-row");
+
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            const colorDetector = document.querySelector(".color-picker");
+
+            colorPicker = colorDetector.value;
+            cell.style.backgroundColor = colorPicker;
+        });
+    });
+}
+
+menu.value = "default";
+gridMaker(gridSize);
+addEventListenerToCells();
